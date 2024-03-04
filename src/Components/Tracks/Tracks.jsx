ed6bd3de58d5c4f034f85db1a9578341ee5f4d87
Vedant-Borkar <122245126+Vedant-Borkar@users.noreply.github.com>
2024-03-04 22:15:21 +0530

    temp

--- src/Components/Tracks/Tracks.jsx
+++ src/Components/Tracks/Tracks.jsx
@@ -4,10 +4,10 @@ import AIML from "../../Assets/AiMlDomain.png";
 import BlockChain from "../../Assets/BlockChainDomain.png";
 import WebApp from "../../Assets/WebDevDomain.png";
 import SocialApp from "../../Assets/SocialDomain.png";
-import VF from "../../Assets/vegetables&fruits.webp";
-import ESS from "../../Assets/essentials.webp";
-import GROCERY from "../../Assets/grocery.jpg";
-import DAIRY from "../../Assets/Dairy.jpg";
+import VF from "../../Assets/Fruits.png";
+import ESS from "../../Assets/Essentials.png";
+import GROCERY from "../../Assets/Grocery1.png";
+import DAIRY from "../../Assets/Diary.png";
 import "./Tracks.css";
 const Tracks = () => {
   return (
